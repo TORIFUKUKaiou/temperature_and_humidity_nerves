ef2e67b5df39c838efa43396f1f062da8886e5e6
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2020-12-31 12:20:14 +0900

    Initial commit

---
+++ lib/temperature_and_humidity_nerves.ex
@@ -0,0 +1,18 @@
+defmodule TemperatureAndHumidityNerves do
+  @moduledoc """
+  Documentation for TemperatureAndHumidityNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TemperatureAndHumidityNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/temperature_and_humidity_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule TemperatureAndHumidityNervesTest do
+  use ExUnit.Case
+  doctest TemperatureAndHumidityNerves
+
+  test "greets the world" do
+    assert TemperatureAndHumidityNerves.hello() == :world
+  end
+end
